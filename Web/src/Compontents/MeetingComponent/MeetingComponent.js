f89aa033ec03d8fec7162a415b2c595819c0794f
levvy13 <mlewandowski1994@wp.pl>
2020-11-21 13:36:04 +0100

    Poprawki
    Poprawki

--- Web/src/Compontents/MeetingComponent/MeetingComponent.js
+++ Web/src/Compontents/MeetingComponent/MeetingComponent.js
@@ -8,16 +8,20 @@ class MeetingComponent extends React.Component {
     render() {
         return (
             <>
-                <Col className="ColMainContentComponent">
-                    <div className="ContainterMeetingComponent">
+                <Col className="ColMainContentComponent" onClick={() => this.props.czyUzupelnionePointy(this.props.meetId)}>
+                    <div className={this.props.point == true ? "test11" : "test22"}>
+                    <Col>
                         <Row className="FirstRowParagraphMainContentComponent">
-                            <p>{this.props.spotkanieTemat}</p>
+                            <p>{this.props.spotkanieTemat} </p>
                         </Row>
                         <Row className="SecondRowParagraphMainContentComponent">
                             <p>{this.props.spotkanieStart}</p>
                         </Row>
+                    </Col>
                     </div>
                 </Col>
+                
+
             </>
         );
     }
---
+++ Web/src/Compontents/SecondMainContentComponentVer2/SecondMainContentComponent.js
@@ -0,0 +1,78 @@
+import './SecondMainContentComponentVer2.css'
+import React, { useState } from 'react';
+import { Col, Row } from 'reactstrap';
+import contentPhoto from '../../Images/geometry-1023846_1920.jpg';
+import contentPhoto2 from '../../Images/school-1019989_1920.jpg';
+import { render } from 'react-dom';
+import Dots from 'react-activity/lib/Dots';
+import 'react-activity/lib/Dots/Dots.css';
+import MeetingComponent from '../MeetingComponent/MeetingComponent';
+import UsersListComponent from '../UsersListComponent/UsersListComponent'
+var tablica;
+
+var axios = require('axios');
+
+class SecondMainContentComponentVer2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { spotkania: [], czyPoint: true, text:"" };
+    }
+changestatus(e){
+    console.log("test change" + JSON.stringify(e.target.id));
+    this.setState(state => ({
+        czyPoint: true,
+        text: e
+    
+
+    }));
+    
+}
+    componentDidMount() {
+        axios.get('http://94.23.91.119:5000/user/GetUserMeetings/true')
+            .then(res => {
+                const spotkania = res.data;
+                spotkania.forEach(element => {
+                    let today = new Date();
+          let startDate = new Date(element.endTime);
+          let diff = startDate.getTime() - today.getTime();
+          console.log(diff + ' ' + element.meetSubject);
+                });
+                this.setState({ spotkania });
+                console.log(spotkania)
+            })
+    }
+    render() {
+        if (this.state.czyPoint) {
+            return (
+                <>
+                    <Row>
+                        <Col sm="9" md="9" lg="9" xl="9">
+                            <Row>
+                                <Col sm="12" md="12" lg="12" xl="12">
+                                    <div className="oknoSpotkanie">
+                                        <p className="StyledPragraph">Oczekiwanie na rozpoczęcie spotkania:</p>
+                                        <p className="StyledPragraph">Oczekiwanie na rozpoczęcie spotkania:</p>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Row>
+
+                                <>
+                                        {this.state.spotkania.map(spotkanie => <Col sm="4" md="4" lg="4" xl="4"><MeetingComponent spotkanieTemat={spotkanie.meetSubject} id={spotkanie.meetId} spotkanieStart={spotkanie.startTime} spotkanieKoniec={spotkanie.endTime} point={spotkanie.isCheckpointsExist} czyUzupelnionePointy={(e) => this.changestatus(e)}/></Col>)}
+                                </>
+
+                            </Row>
+                        </Col>
+                        <Col >
+                            <UsersListComponent />
+                        </Col>
+                    </Row>
+
+                </>
+            )
+
+        }
+
+    }
+}
+export default SecondMainContentComponentVer2
