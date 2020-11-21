34371c3cf52f8448e643274257709c3b3ae65c71
o-szymanski <osk.szymanski@gmail.com>
2020-11-21 12:11:05 +0100

    Edit Files Add Logo Change Names

--- Web/src/Compontents/MainContentComponent/MainContentComponent.js
+++ Web/src/Compontents/MainContentComponent/MainContentComponent.js
@@ -1,10 +1,10 @@
 import './MainContentComponent.css'
 import React, { useState } from 'react';
 import { Col, Row } from 'reactstrap';
-import contentPhoto from '../../Images/geometry-1023846_1920.jpg';
+import contentPhoto from '../../Images/geometry.jpg';
 import LoginButtonComponent from '../LoginButtonComponent/LoginButtonComponent';
-import contentPhoto2 from '../../Images/school-1019989_1920.jpg';
-import microsoftAccount from '../../Images/Mask Group 1.png'
+import contentPhoto2 from '../../Images/school.jpg';
+import microsoftAccount from '../../Images/mask.png'
 class MainContentComponent extends React.Component {
     constructor(props) {
         super(props);
